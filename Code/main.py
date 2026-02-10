28942f3fff2fb20aab4d8d2d231d9fd9986bd421
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2026-02-10 21:08:55 +0000

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,12 +1,22 @@
 # main.py
 import sys
-
 from PySide6.QtWidgets import QApplication
-
 from main_window import TextAnimatorWindow
+# from utils.logging import log_message
+
+appname = "Cozy Animator"
+
+# log_message("Cozy Animator")
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = TextAnimatorWindow()
-    window.show()
-    sys.exit(app.exec())
+    try:
+        log_message( "%s launched" % appname )
+        app = QApplication(sys.argv)
+        app.setStyle("Fusion")
+        window = TextAnimatorWindow()
+        window.show()
+        sys.exit(app.exec())
+    except Exception as e:
+        # log_message(f"Starting %s catastrophically failed: {str(e)}" % appname, level="CRITICAL")
+        print(f"%s has entered the void: {str(e)}" % appname, file=sys.stderr)
+        sys.exit(1)
